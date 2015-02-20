9877f80144996bf0ec7590f832924180d5e1dcad
happyEyes <sabatominco@gmail.com>
2015-02-20 13:25:25 +0100

    Add class Immagine

---
+++ src/modello/Immagine.java
@@ -0,0 +1,10 @@
+public package modello;
+
+public class Immagine{
+
+    private BufferedImage originalImage;
+    private int heightImage;
+    private int widthImage;
+    private List<Filter> list = new ArrayList<Filtri>();
+
+}
