90052701d8ee555c541147d176b60ada174d0547
happyEyes <sabatominco@gmail.com>
2015-02-20 12:57:34 +0100

    New Class Modello

---
+++ src/model/Modello.java
@@ -0,0 +1,24 @@
+package model;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Modello {
+
+    private Map<String, Object> mappaBean = new HashMap<String, Object>();
+
+    public Modello() {
+   
+    }
+
+    public Object getBean(String nome) {
+        return mappaBean.get(nome);
+    }
+
+    public void putBean(String nome, Object object) {
+        this.mappaBean.put(nome, object);
+    }
+}
